32a2a95a4a037bd9cd394cdf3d4098ebc59515b3
Peter <hyu067@gmail.com>
2017-04-25 12:21:24 +1200

    Changed Updated to Insert for clean DB deployment

--- sql/updates/world/3.3.5/2017_04_19_00_world.sql
+++ sql/updates/world/3.3.5/2017_04_19_00_world.sql
@@ -1,3 +1,5 @@
 -- TDB 335.63 world
-UPDATE `version` SET `db_version`='TDB 335.63', `cache_id`=63 LIMIT 1;
+DELETE FROM `version`;
+INSERT INTO `version` (`core_version`, `core_revision`, `db_version`, `cache_id`) VALUES
+('TrinityCore rev. 11104d44e1ca 2017-04-24 15:26:48 +1200 (3.3.5 branch) (Win64, Release)','11104d44e1ca','TDB 335.63',63);
 UPDATE `updates` SET `state`='ARCHIVED';
